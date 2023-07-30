96934719d9c8df1c54cc1ec4d55922b993442809
Ivan Esmantovich <ivanesmantovich@gmail.com>
2023-07-30 22:53:38 +0300

    Static previews ready

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,18 +1,76 @@
 import styles from './Projects.module.css'
+import screenshot from '@/public/screenshot.png'
+import appStoreDownload from '@/public/appStoreDownload.svg'
 
-import Link from 'next/link'
 import Snowdrop from './Snowdrop'
 import Preview from './Preview'
+import Image from 'next/image'
 
 export default function Projects() {
     return (
-        <div
-            className={`flex grow flex-col border-2 border-black ${styles.fadeIn}`}
-        >
-            <div className="self-center flex grow flex-wrap items-center justify-between w-8/12 border-2 border-emerald-300">
-                <div className={`${styles.preview}`}></div>
-                <div className={`${styles.preview}`}></div>
-                <div className={`${styles.preview}`}></div>
+        <div className={`flex grow flex-col ${styles.fadeIn}`}>
+            <div className="self-center flex grow flex-wrap items-center justify-between w-2/3">
+                <div className={`${styles.previewContainer} ${styles.enlarge}`}>
+                    <div className={`${styles.preview}`}>
+                        <Image alt="" src={screenshot} fill={true} />
+                    </div>
+                    <div
+                        className={`flex grow flex-col items-start pl-8 text-sm text-sm border-t border-gray-300`}
+                    >
+                        <div className="py-2">
+                            <div>Reminders</div>
+                            <div className="font-semibold">
+                                App that reminds you stuff
+                            </div>
+                        </div>
+                        <Image
+                            alt=""
+                            src={appStoreDownload}
+                            height={40}
+                            width={119}
+                        />
+                    </div>
+                </div>
+
+                <div className={`${styles.previewContainer} ${styles.enlarge}`}>
+                    <div className={`${styles.preview}`}>
+                        <Image alt="" src={screenshot} fill={true} />
+                    </div>
+                    <div className="flex grow flex-col items-start pl-8 text-sm text-sm border-t border-gray-300">
+                        <div className="py-2">
+                            <div>Reminders</div>
+                            <div className="font-semibold">
+                                App that reminds you stuff
+                            </div>
+                        </div>
+                        <Image
+                            alt=""
+                            src={appStoreDownload}
+                            height={40}
+                            width={119}
+                        />
+                    </div>
+                </div>
+
+                <div className={`${styles.previewContainer} ${styles.enlarge}`}>
+                    <div className={`${styles.preview}`}>
+                        <Image alt="" src={screenshot} fill={true} />
+                    </div>
+                    <div className="flex grow flex-col items-start pl-8 text-sm border-t border-gray-300">
+                        <div className="py-2">
+                            <div>Reminders</div>
+                            <div className="font-semibold">
+                                App that reminds you stuff
+                            </div>
+                        </div>
+                        <Image
+                            alt=""
+                            src={appStoreDownload}
+                            height={40}
+                            width={119}
+                        />
+                    </div>
+                </div>
             </div>
         </div>
     )
