47aa50b0fd87aa5d9b1207d101e692810e511b71
Ivan Esmantovich <ivanesmantovich@gmail.com>
2023-09-23 17:43:04 +0300

    Edit about

--- components/About.tsx
+++ components/About.tsx
@@ -15,23 +15,9 @@ export default function About() {
             </Link>
 
             <div className="mt-32 mb-60 text-3xl">
-                <div className="mb-1">My name is Ivan.</div>
-                <div className="mb-1">
-                    I develop in{' '}
-                    <span
-                        className={`${styles.bgTransition} ${styles.bgSwift} text-white py-0.5 px-1 rounded cursor-pointer`}
-                    >
-                        Swift
-                    </span>{' '}
-                    and {/* TODO: Сделать ссылками */}
-                    <span
-                        className={`${styles.bgTransition} ${styles.bgReact} text-white py-0.5 px-1 rounded cursor-pointer`}
-                    >
-                        React
-                    </span>
-                    .
-                </div>
-                <div className="mb-1">I love good UX and good Design.</div>
+                <div className="mb-1">My name is Ivan Esmantovich</div>
+                <div className="mb-1">I strive to make aesthetic, unobtrusive, <span className='underline'>useful</span> products</div>
+                <div className="mb-1">I love the Internet</div>
                 <div className="flex">
                     <div>I am currently interested in&nbsp;</div>
                     <Interests />
